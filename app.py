2e12d1b48ee82fca42b3fd57a2a80e9adb82719e
(hojomu) <(whdan45@gmail.com)>
2023-05-22 00:13:32 +0900

    config 및 gitignore 추가

--- app.py
+++ app.py
@@ -3,31 +3,55 @@ from flask_mysqldb import MySQL
 from GeolcationController import GeolcationController
 from MedicalMemberController import MedicalMemberController
 from LocationDataController import LocationDataController
-from moveController import moveController
+import config
 
 app = Flask(__name__)
+# config 객체 생성
+app.config.from_object(config)
 
 # MySQL 연결 설정
-app.config['MYSQL_HOST'] = 'localhost'  # MySQL 호스트 주소
-app.config['MYSQL_PORT'] = 3306
-app.config['MYSQL_USER'] = 'root'  # MySQL 사용자명
-app.config['MYSQL_PASSWORD'] = '0000'  # MySQL 비밀번호
-app.config['MYSQL_DB'] = 'control_second_project'  # 사용할 데이터베이스명
+app.config['MYSQL_HOST'] = config.MYSQL_HOST  # MySQL 호스트 주소
+app.config['MYSQL_PORT'] = config.MYSQL_PORT
+app.config['MYSQL_USER'] = config.MYSQL_USER  # MySQL 사용자명
+app.config['MYSQL_PASSWORD'] = config.MYSQL_PASSWORD  # MySQL 비밀번호
+app.config['MYSQL_DB'] = config.MYSQL_DB  # 사용할 데이터베이스명
+
 
 # MySQL 초기화
 mysql = MySQL(app)
+
 
 # DB 연결 블루 프린트 ( CONTROLLER )
 app.register_blueprint(GeolcationController)
 app.register_blueprint(MedicalMemberController)
 app.register_blueprint(LocationDataController)
 
-# 이동용 블루 프린트 ( 삭제 예정 )
-app.register_blueprint(moveController)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+# 이동용 route
+@app.route('/Geolocation')
+def Geolocation():
+    return render_template('Geolocation.html')
+
+@app.route('/locationManagement')
+def locationManagement():
+    KAKAOMAP_API_KEY = app.config['KAKAOMAP_API_KEY']
+    return render_template('locationManagement.html', KAKAOMAP_API_KEY = KAKAOMAP_API_KEY)
+
+@app.route('/member')
+def member():
+    return render_template('member.html')
+
+@app.route('/a')
+def a():
+    KAKAOMAP_API_KEY = app.config['KAKAOMAP_API_KEY']
+    return render_template('a.html', KAKAOMAP_API_KEY = KAKAOMAP_API_KEY)
+# 이동용 route 끝
+
 
 if __name__ == '__main__':
     app.run()
